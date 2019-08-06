0f918de0228cc05905b257e5f76265a707bd50c2
Nadia Kaloyanova <nadia@technology-valley.com>
2019-08-06 13:17:17 +0300

    Implementing the list of causes.

--- src/views/cause-list.vue
+++ src/views/cause-list.vue
@@ -1,13 +1,79 @@
 <template>
-    <div class="pa-5">
-        <h1>Causes</h1>
-    </div>
+    <v-container grid-list-lg>
+        <v-layout row wrap>
+            <v-flex xs12>
+                <h1>Causes</h1>
+                <p>Choose the causes that affect you and you would like to fight for. It is time to demand your rights!</p>
+            </v-flex>
+        </v-layout>
+        <v-layout row wrap>
+            <v-flex xs12 md3 v-for="cause in causes" :key="cause.id">
+                <v-card>
+                    <v-img
+                        height="200px"
+                        :src="cause.imageUrl"
+                    />
+                    <v-card-title>{{ cause.title }}</v-card-title>
+                    <v-card-text>{{ truncate(cause.description) }}</v-card-text>
+                    <v-card-actions>
+                        <v-btn color="primary mr-1" small>Learn more</v-btn>
+                        <v-btn @click="join(cause)" color="primary" small>Join</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
+        </v-layout>
+    </v-container>
 </template>
 
 <script>
-  export default {
-      name: "CauseList",
+    import * as ellipsize from 'ellipsize';
+    import {mapGetters, mapMutations, mapActions} from "vuex";
+    import causes from '../assets/causes.json';
 
-      components: {}
-  };
+    export default {
+        name: "CauseList",
+
+        components: {},
+
+        data: () => ({
+            causes: causes
+        }),
+
+        computed: {
+            ...mapGetters([
+                'session',
+                'username'
+            ]),
+        },
+
+        methods: {
+            truncate(text) {
+                return ellipsize(text, 100);
+            },
+
+            join(cause) {
+                this.session
+                    .getFile('/causes.txt', {decrypt: false})
+                    .then((input) => {
+                        let data = {};
+
+                        if (input) {
+                            data = JSON.parse(input);
+                        }
+
+                        data[cause.id] = true;
+
+                        return this.session.putFile(
+                            '/causes.txt',
+                            JSON.stringify(data),
+                            {encrypt: false}
+                        );
+                    })
+                    .then(() => {
+                        console.log(`Joined to ${cause.id}.`);
+                    })
+                ;
+            }
+        }
+    };
 </script>
