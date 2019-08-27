fca42c234278759af1da6bbeaf0389f5342fbac2
Nadia Kaloyanova <nadia@technology-valley.com>
2019-08-27 19:11:29 +0300

    Improving the authentication logic.

--- src/router.js
+++ src/router.js
@@ -3,24 +3,52 @@ import Router from 'vue-router'
 import SignIn from './views/sign-in.vue'
 import CauseList from './views/cause-list.vue'
 import MyCauses from './views/my-causes.vue'
+import store from './store';
 
 Vue.use(Router)
 
-export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      component: CauseList
-    },
-    {
-        path: '/my',
-        component: MyCauses
-    },
-    {
-      path: '/sign-in',
-      component: SignIn
-    },
-  ]
+const router = new Router({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            component: CauseList,
+            meta: {
+                protected: false
+            }
+        },
+        {
+            path: '/my',
+            component: MyCauses,
+            meta: {
+                protected: true
+            }
+        },
+        {
+            path: '/sign-in',
+            component: SignIn,
+            meta: {
+                protected: false
+            }
+        },
+    ]
 })
+
+router.beforeEach((to, from, next) => {
+    const isProtected = to.meta.protected || false;
+
+    if (isProtected) {
+        if (store.state.username) {
+            next();
+        }
+        else {
+            next('/sign-in');
+        }
+    }
+    else {
+        next();
+    }
+});
+
+export default router;
